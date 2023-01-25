65deffdc09210420b5dd62989e27a82f86891c06
Md. Sakhawat Hossain <biplobbms@gmail.com>
2023-01-25 13:35:11 +0600

    getProducts API successfully run

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,11 @@
-const express = require('express')
+const express = require('express');
 const app = express()
+app.use(express.json());
+
+// Import the routes 
+const products = require('./routes/product');
+
+app.use('/api/v1', products)
+
 
 module.exports = app
